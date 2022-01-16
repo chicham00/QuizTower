b77096199fc7a76ac95e2580ee430c1792a0bb50
chicham00 <62507741+chicham00@users.noreply.github.com>
2022-01-17 01:30:48 +0900

    20220117_複数の答えを保持するを実装

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -7,55 +7,58 @@ use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
+
     /**
-     * 
+     *
      * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
      */
-
     public function index()
     {
         $categories = Category::get();
-        return view('top',compact('categories'));
+        return view('top', compact('categories'));
     }
+
     /**
-     * 
-     * @param $id
+     *
+     * @param
+     *            $id
      * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
      */
-    
     public function question($category_id)
     {
-        //var_dump($category_id);
-        
+        // var_dump($category_id);
+
         // DBから押されたカテゴリに該当する問題をランダムに１件取得し変数"quiz"へ代入
-        $quiz = Quiz::where('category_id',$category_id)->inRandomOrder()->first();
-        
+        $quiz = Quiz::where('category_id', $category_id)->inRandomOrder()->first();
+
         // DBから問題をランダムに１件取得し変数"quiz"へ代入
-        //$quiz = Quiz::inRandomOrder()->first();
-        
+        // $quiz = Quiz::inRandomOrder()->first();
+
         // 代入した値をview'question'へ送る
         return view('question', compact('quiz'));
     }
 
-    /**
-     * 
-     */
     // Request 変数名:前の画面から送られてきた値が入っている
     public function answer(Request $request)
     {
-        $answer = $request->input('answer');
+        // ユーザーが入力した値をanswerに代入
+        $inputanswer = $request->input('answer');
         $id = $request->input('quizid');
-        // $quiz = Quiz::inRandomOrder()->first();
 
         $quiz = Quiz::find($id);
-        $quiz->answer;
+
+        // 取得したanswerを配列に変換する
+        $answers = explode(",", $quiz->answer);
+        // input('answer')と配列に直したanswersを比較するためにforeachを使用
+
         // 初めにfalseを定義
         $result = false;
-        if ($answer == $quiz->answer) {
-            $result = true;
+
+        foreach ($answers as $ans) {
+            if ($inputanswer == $ans) {
+                $result = true;
+            }
         }
-        // 任意の値を表示させる
-         //var_dump($categories);
 
         return view('answer', compact('result'));
     }
--- database/seeds/QuizzesTableSeeder.php
+++ database/seeds/QuizzesTableSeeder.php
@@ -15,22 +15,22 @@ class QuizzesTableSeeder extends Seeder
         DB::table('quizzes')->insert([
             'category_id' => '1',
             'question' => '日本で一番高い山は？',
-            'answer' => '富士山'
+            'answer' => '富士山,ふじさん'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '1',
             'question' => 'パンはパンでも食べられないパンはなーんだ？',
-            'answer' => 'フライパン'
+            'answer' => 'フライパン,ふらいぱん'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '1',
             'question' => '本屋さんで買いたいのに買えない本のジャンルってなーんだ？',
-            'answer' => '占い'
+            'answer' => '占い,うらない'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '1',
             'question' => '「何でも増産して増やすぞ‼︎」という動物は？',
-            'answer' => 'ぞう'
+            'answer' => 'ぞう,象,ゾウ'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '1',
@@ -40,27 +40,27 @@ class QuizzesTableSeeder extends Seeder
         DB::table('quizzes')->insert([
             'category_id' => '2',
             'question' => 'すぐに老朽化してしまう車は？',
-            'answer' => 'レッカー車'
+            'answer' => 'レッカー車,れっかーしゃ'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '1',
             'question' => '切っても切ってもきれないものってなーんだ？',
-            'answer' => 'トランプ'
+            'answer' => 'トランプ,とらんぷ'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '1',
             'question' => '天気が悪いと見えなくて、天気がいいとよく見える。夕方になればグーンと背伸び！夜になったらさようなら。これなーんだ？',
-            'answer' => 'かげ'
+            'answer' => 'かげ,影,カゲ'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '2',
             'question' => '目を開けていると見えなくて、目を閉じると見えるものってなーんだ？',
-            'answer' => 'ゆめ'
+            'answer' => 'ゆめ,夢'
         ]);
         DB::table('quizzes')->insert([
             'category_id' => '1',
             'question' => '鹿さんがお医者さんになったよ。さて、どんなお医者さんになったでしょうか？',
-            'answer' => 'しかい'
+            'answer' => 'しかい,歯医者,歯科医'
         ]);
     }
 }
